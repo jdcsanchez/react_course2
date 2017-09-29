6470ce3356b6d735b77bb544779480550da80e1f
jdiego82 <jdiego82@yahoo.com>
2017-09-29 22:16:06 +0800

    Finished part 3

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import BookDetail from '../containers/book-detail';
+import BookList from '../containers/book-list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <BookList />
+        <BookDetail />
+      </div>
     );
   }
 }
